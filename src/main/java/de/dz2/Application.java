001d121a06ed08a42e271e92a6c785b2457d7cd8
Ayrat <ayrat1@mail.ru>
2021-03-20 09:30:17 +0300

    init project

---
+++ src/main/java/de/dz2/Application.java
@@ -0,0 +1,13 @@
+package de.dz2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
